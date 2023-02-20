c895b6eca63e8d45f588c69feba72e05fb9defb3
sun0o0o <sy.choi1106@gmail.com>
2023-02-20 18:21:04 +0900

    fix:README.md 작성

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Data-Structures-and-Algorithms
+
 Data Structures and Algorithms with JavaScript
+
+<img src="https://user-images.githubusercontent.com/85756211/220063813-a8d7f34c-0fe3-413d-98ac-d0ec6251de81.jpg"/>
+
+1. 자바스크립트 프로그래밍 환경과 모델
+2. 배열
+3. 리스트
+4. 스택
+5. 큐
+6. 연결 리스트
+7. 딕셔너리
+8. 해싱
+9. 집합
+10. 이진 트리와 이진 검색 트리
+11. 그래프와 그래프 알고리즘
+12. 정렬 알고리즘
+13. 검색 알고리즘
+14. 고급 알고리즘
---
+++ unit1/exam1.12.js
@@ -0,0 +1,33 @@
+// 객체는 생성자 함수를 이용해 정의한다.
+//생성자 함수는 객체의 프로퍼티, 함수 선언, 함수 정의를 포함한다.
+function Checking(amount) {
+  this.balance = amount; //property
+  this.deposit = deposit; // 함수
+  this.withdraw = withdraw; //함수
+  this.toString = toString; //함수
+}
+
+function deposit(amount) {
+  this.balance += amount;
+}
+
+function withdraw(amount) {
+  if (amount <= this.balance) {
+    this.balance -= amount;
+  }
+  if (amount > this.balance) {
+    console.log("Insufficient funds");
+  }
+}
+
+function toString() {
+  return "Balance: " + this.balance;
+}
+
+let account = new Checking(500);
+account.deposit(1000);
+console.log("after deposit", account.toString());
+account.withdraw(750);
+console.log("after withdraw", account.toString());
+account.withdraw(800);
+console.log("after withdraw2", account.toString());
